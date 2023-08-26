ee340e33c1aa9092bfc7ee1f507502ba44d87895
Hideki Yoshizane <hideki.yoshizane04@gmail.com>
2023-08-25 21:34:22 -0300

    Server dropdown fully implemented

--- public/javascript/home.js
+++ public/javascript/home.js
@@ -1,65 +1,166 @@
 document.addEventListener('DOMContentLoaded', async () => {
   try {
-    const response = await fetch('/userServer');
-    const data = await response.json();
+    const data = await fetchData('/userServer');
 
     const serverSelector = document.getElementById('serverSelector');
-    const dropdownToggle = document.querySelector('.selectedServer');
+    const selectedServer  = document.querySelector('.selectedServer');
+
     let isFirstButton = true;
+    const savedServerContent = getCookie('selectedServerContent');
 
     for (const serverID of data) {
       try {
-        const idAnswer = await fetch(`/serverName/${serverID}`);
-        const serverData = await idAnswer.json();
+        const serverData = await fetchData(`/serverName/${serverID}`);
+        const createdButton = createServerButton(serverData);
 
-        const serverButton = document.createElement('button');
+
         if(isFirstButton){
-          serverButton.classList.add('SelectedButton'); 
-        }
-        else{
-          serverButton.classList.add('serverButton');
-        } 
+          createdSelectedButton = createdButton.cloneNode(true);
+          createdSelectedButton.classList.replace('serverButton', 'SelectedButton');
 
-        const serverImage = document.createElement('img');
-        serverImage.src = `${serverData.img}.webp`;
-        serverImage.alt = serverData.name;
-        serverImage.classList.add('serverIcon');
-        serverButton.appendChild(serverImage);
+          const svgElement = createdSelectedButton.querySelector('svg');
+          createdSelectedButton.removeChild(svgElement);
+          
+          selectedServer.insertBefore(createdSelectedButton, selectedServer.firstChild);
+          createdSelectedButton.classList.toggle('not-checked');
 
-        const serverNameSpan = document.createElement('span');
-        serverNameSpan.textContent = serverData.name;
-        serverButton.appendChild(serverNameSpan);
-        if (isFirstButton) {
-          dropdownToggle.insertBefore(serverButton, dropdownToggle.firstChild);
+          updateToCookie(selectedServer, savedServerContent);
           isFirstButton = false;
-        } else {
-          serverSelector.appendChild(serverButton);
         }
+
+        if(createdButton.querySelector('span').textContent === savedServerContent){
+          createdButton.classList.toggle('not-checked');
+          createdButton.classList.toggle('checked');
+        }
+
+        serverSelector.appendChild(createdButton);
 
       } catch (error) {
         console.error('Error fetching server name:', error);
       }
     }
+
+    const serverButtons = serverSelector.querySelectorAll('.serverButton');
+    let selectedButton = dropdownToggle.querySelector('.SelectedButton');
+    
+serverButtons.forEach(serverButton => {
+  serverButton.addEventListener('click', () => {
+    serverButtons.forEach(button => {
+      if (button !== serverButton) {
+        button.classList.add('not-checked');
+        button.classList.remove('checked');
+      }
+    });
+
+    swapContentAndStoreCookie(selectedButton, serverButton);
+    serverButton.classList.toggle('not-checked');
+    serverButton.classList.toggle('checked');
+  });
+});
+    
+
   } catch (error) {
     console.error('Error fetching user data:', error);
   }
 });
 
-(function() {
+async function fetchData(url) {
+  const response = await fetch(url);
+  return response.json();
+}
+
+function createServerButton(serverData) {
+  const createdButton = document.createElement('button');
+  createdButton.classList.add('serverButton');
+
+
+  const checkSVG = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+  checkSVG.setAttribute('width', '30');
+  checkSVG.setAttribute('height', '30');
+  checkSVG.setAttribute('viewBox', '0 0 36 27');
+  checkSVG.setAttribute('fill', 'none');
+
+  const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+  path.setAttribute('d', 'M12 21.4L3.59999 13L0.799988 15.8L12 27L36 2.99995L33.2 0.199951L12 21.4Z');
+  path.setAttribute('fill', '#E3E3E3');
+
+
+  checkSVG.appendChild(path);
+
+
+  const serverImage = document.createElement('img');
+  serverImage.src = `${serverData.img}.webp`;
+  serverImage.alt = serverData.name;
+  serverImage.classList.add('serverIcon');
+  createdButton.appendChild(serverImage);
+
+  const serverNameSpan = document.createElement('span');
+  serverNameSpan.textContent = serverData.name;
+  createdButton.appendChild(serverNameSpan);
+  createdButton.appendChild(checkSVG);
+  createdButton.classList.toggle('not-checked');
+  return createdButton;
+
+}
+
+function swapContentAndStoreCookie(selectedButton, serverButton) {
+  const selectedImage = selectedButton.querySelector('img');
+  const selectedName = selectedButton.querySelector('span');
+
+  const serverImage = serverButton.querySelector('img');
+  const serverNameSpan = serverButton.querySelector('span');
+
+
+  selectedImage.setAttribute('alt', serverNameSpan.textContent);
+  selectedImage.src = serverImage.src;
+  selectedName.textContent = serverNameSpan.textContent;
+
+  setCookie('selectedServerContent', serverNameSpan.textContent);
+
+}
+
+
+(function setupDropdownToggle() {
   const dropdownToggle = document.querySelector('.dropdownToggle');
-  const svgIcon = dropdownToggle.querySelector('.icon'); // Replace with correct selector
+  const svgIcon = dropdownToggle.querySelector('.icon');
   let isOpen = false;
 
   dropdownToggle.addEventListener('click', function() {
-    if (isOpen) {
-      dropdownToggle.classList.remove('open');
-      dropdownToggle.classList.add('closed');
-      svgIcon.classList.remove('rotate');
-    } else {
-      dropdownToggle.classList.remove('closed');
-      dropdownToggle.classList.add('open');
-      svgIcon.classList.add('rotate');
-    }
     isOpen = !isOpen;
+    dropdownToggle.classList.toggle('open', isOpen);
+    dropdownToggle.classList.toggle('closed', !isOpen);
+    svgIcon.classList.toggle('rotate', isOpen);
   });
 })();
+
+
+
+
+function setCookie(name, value, days) {
+  const expires = new Date();
+  expires.setTime(expires.getTime() + (days * 24 * 60 * 60 * 1000));
+  document.cookie = `${name}=${encodeURIComponent(value)};expires=${expires.toUTCString()}`;
+}
+
+
+function getCookie(name) {
+  const cookieArr = document.cookie.split(';');
+  for (let i = 0; i < cookieArr.length; i++) {
+    const cookiePair = cookieArr[i].split('=');
+    if (cookiePair[0].trim() === name) {
+      return decodeURIComponent(cookiePair[1]);
+    }
+  }
+  return null;
+}
+
+function updateToCookie(selectedServer, savedServerContent){
+  const selectedServerImg = selectedServer.querySelector('img');
+  const currentImgSrc = selectedServerImg.getAttribute('src');
+  const newImgSrc = currentImgSrc.replace(/[^/]*\.webp$/, `${savedServerContent}.webp`);
+  selectedServerImg.src = newImgSrc;
+  selectedServerImg.setAttribute('alt', savedServerContent);
+  selectedServer.querySelector('span').textContent = savedServerContent;
+
+}
+
