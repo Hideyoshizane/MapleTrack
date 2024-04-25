1abb4b1b8a0e5dddcb7a6d9fca50fd3d699f3565
Hideki Yoshizane <40189933+Hideyoshizane@users.noreply.github.com>
2024-04-25 15:13:15 -0300

    Update 1.2.3
    Login bug fix

--- controllers/authController.js
+++ controllers/authController.js
@@ -11,30 +11,33 @@ require('dotenv').config();
 module.exports = {
   login: async (req, res) => {
     try {
-      const username = req.body.username;
+      const { username, password } = req.body;
       const user = await User.findOne({ username });
-      const passwordMatch = await bcrypt.compare(req.body.password, user.password);
- 
-      if (!user || !passwordMatch) {
+      if (!user) {
         req.flash('message', 'Wrong username or password.');
         req.flash('type', 'error');
         return res.redirect('/login');
-      } else {
-        
-
-        if (user.version < LASTVERSION) {
-          await searchServersAndCreateMissing(user, user._id);
-          await createMissingCharacters(user._id, user.username);
-          await updateCharacters(user._id);
-          await updateUserVersion(user._id);
-        }
-
-        await updateLastLogin(user._id);
-        await resetBossList(user.username);
-        const token = jwt.sign({ _id: user._id, username: user.username }, process.env.SECRET, { expiresIn: '7d' });
-        res.cookie('token', token, { httpOnly: true, maxAge: 604800000 });
-        res.redirect('/home');
       }
+
+      const passwordMatch = await bcrypt.compare(password, user.password);
+      if (!passwordMatch) {
+        req.flash('message', 'Wrong username or password.');
+        req.flash('type', 'error');
+        return res.redirect('/login');
+      }
+
+      if(user.version < LASTVERSION) {
+        await searchServersAndCreateMissing(user, user._id);
+        await createMissingCharacters(user._id, user.username);
+        await updateCharacters(user._id);
+        await updateUserVersion(user._id);
+      }
+
+      await updateLastLogin(user._id);
+      await resetBossList(user.username);
+      const token = jwt.sign({ _id: user._id, username: user.username }, process.env.SECRET, { expiresIn: '7d' });
+      res.cookie('token', token, { httpOnly: true, maxAge: 604800000 });
+      res.redirect('/home');
     } catch (err) {
       console.error('Error during login:', err);
       res.status(500).json({ error: 'An error occurred during login' });
