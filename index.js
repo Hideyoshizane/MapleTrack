cd55e0c92ae23d29691181fdae1c84d5108f350b
Hideki Yoshizane <40189933+Hideyoshizane@users.noreply.github.com>
2024-10-07 00:20:07 -0300

    bufix

--- index.js
+++ index.js
@@ -1,45 +1,39 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const session = require('express-session');
+const expressSession = require('express-session');
 const flash = require('express-flash');
 const mongoose = require('mongoose');
 const path = require('path');
 const router = require('./routes');
 const MongoStore = require('connect-mongo');
+
 require('dotenv').config();
 
-const app = express();
 const DB_URL = process.env.DB_URL || 'mongodb://localhost:27017';
-const PORT = process.env.PORT || 8080;
+const app = express();
 
 const mongoOptions = {
 	serverSelectionTimeoutMS: 10000,
 	socketTimeoutMS: 20000,
 };
 
-// Connect to MongoDB
+// MongoDB connection
 mongoose
 	.connect(DB_URL, mongoOptions)
-	.then(() => console.log('Database connected.'))
+	.then(() => {
+		console.log('Database connected.');
+	})
 	.catch((err) => {
-		console.error('Error connecting to MongoDB:', err);
+		console.error('Error connecting to MongoDB', err);
 		process.exit(1);
 	});
 
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.on('error', console.error.bind(console, 'connection error:'));
 
-// Middleware setup
-app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
-app.use(flash());
-app.use(express.static(path.join(__dirname, 'public'), { maxAge: '1w' }));
-
-// Session setup
 app.use(
-	session({
+	expressSession({
 		secret: process.env.SECRET || 'random',
 		resave: false,
 		saveUninitialized: false,
@@ -51,19 +45,22 @@ app.use(
 	})
 );
 
-// View engine setup
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(flash());
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname)));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, 'public')));
 
-// Cache control middleware
 app.use((req, res, next) => {
 	const isImageRequest = /\.(webp)$/.test(req.path);
 
 	if (req.headers['x-force-cache'] === 'true') {
 		res.setHeader('Cache-Control', 'public, max-age=604800'); // Cache for one week
 	} else if (!isImageRequest) {
-		// No-cache headers for non-image files
+		// Set no-cache headers for non-image files
 		res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0');
 		res.setHeader('Pragma', 'no-cache');
 		res.setHeader('Expires', '0');
@@ -71,10 +68,11 @@ app.use((req, res, next) => {
 	next();
 });
 
-// Routes
+app.use(express.static(path.join(__dirname, 'public'), { maxAge: 604800000 }));
+
 app.use('/', router);
 
-// Start the server
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
 	console.log(`Server is listening on port ${PORT}.`);
 });
