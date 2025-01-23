f19e953c8b4bbf0ff2dcaaecece3c26921cb23b0
Hideki Yoshizane <40189933+Hideyoshizane@users.noreply.github.com>
2025-01-23 00:30:01 -0300

    update 1.10.5
    -app version location update

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ require('dotenv').config();
 const DB_URL = process.env.DB_URL || 'mongodb://localhost:27017';
 const app = express();
 
-app.locals.globalVariable = '1.10.4';
+app.locals.globalVariable = '1.10.5';
 
 const mongoOptions = {
 	serverSelectionTimeoutMS: 10000,
