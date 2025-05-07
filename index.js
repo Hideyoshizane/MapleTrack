a7baacd8bbc0cd495c15748d7e25366f4044e12a
Hideki Yoshizane <hideki.yoshizane04@gmail.com>
2025-05-06 23:13:11 -0300

    update 1.12.2
    -CSS bugfixes

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ require('dotenv').config();
 const DB_URL = process.env.DB_URL || 'mongodb://localhost:27017';
 const app = express();
 
-app.locals.globalVariable = '1.12.1';
+app.locals.globalVariable = '1.12.2';
 
 const mongoOptions = {
 	serverSelectionTimeoutMS: 10000,
